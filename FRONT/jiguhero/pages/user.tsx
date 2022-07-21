7daf664faa5b60b195fee49b93eac22c05e8d392
유이서 <yoojh456@naver.com>
2022-07-21 22:21:22 +0900

    FE/login MUI component 추가

--- FRONT/jiguhero/pages/user.tsx
+++ FRONT/jiguhero/pages/user.tsx
@@ -2,7 +2,12 @@ import Head from "next/head"
 import type { NextPage } from 'next'
 import React from "react";
 import TextField  from "@mui/material/TextField";
-import InputLabel from '@material-ui/core/InputLabel';
+import IconButton from '@mui/material/IconButton';
+import PhotoCamera from '@mui/icons-material/PhotoCamera';
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
+import Box from '@mui/material/Box'
+
 
 const User: NextPage = ()=>{
     return(
@@ -12,10 +17,24 @@ const User: NextPage = ()=>{
         </Head>
 
         <main>
-            <h1>hi hello</h1>
-            <TextField label="닉네임을 작성해 주세요" required />
+            <h1>회원가입</h1>
+            <Stack direction="row" alignItems="center" spacing={2}>
+                <IconButton color="primary" aria-label="upload picture" component="label">
+                    <input hidden accept="image/*" type="file" />
+                    <PhotoCamera />
+                </IconButton>
+            </Stack>
+            <div>
+                사용하실 대원명을 입력해주세요
+            </div>
+            <Box>
+                <TextField id="standard-required" label="닉네임을 작성해 주세요" reuired variant="standard" />  
+                <Button variant="contained">중복확인</Button>
+           </Box>
             {/* <TextField error helperText="10자 이내로 작성해 주세요!" /> */}
- 
+            <Stack spacing={2} direction="row">
+                <Button variant="contained">가입완료</Button>
+            </Stack>
         </main>
     </div>
     );
