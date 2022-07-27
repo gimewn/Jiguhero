e9da047b9db4fa362879c193c76140f91b77a481
이기영 <kian6365@naver.com>
2022-07-27 13:10:42 +0900

    S07P12C105 / add func naver login

---
+++ BACK/jiguhero/src/main/java/com/ssafy/jiguhero/TestApi.java
@@ -0,0 +1,12 @@
+package com.ssafy.jiguhero;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestApi {
+    @GetMapping("/test")
+    public String index() {
+        return "Hello World";
+    }
+}
