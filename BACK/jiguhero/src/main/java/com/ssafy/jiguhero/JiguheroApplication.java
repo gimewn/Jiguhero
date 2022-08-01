dc1f020fdef2ddae8f4cdbb48c115f7486c39eea
Yeongseo Kim <dudtjakdl@naver.com>
2022-08-01 14:57:49 +0900

    merge

--- BACK/jiguhero/src/main/java/com/ssafy/jiguhero/JiguheroApplication.java
+++ BACK/jiguhero/src/main/java/com/ssafy/jiguhero/JiguheroApplication.java
@@ -10,6 +10,7 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 public class JiguheroApplication {
 
 	public static void main(String[] args) {
+		System.out.println();
 		SpringApplication.run(JiguheroApplication.class, args);
 	}
 
