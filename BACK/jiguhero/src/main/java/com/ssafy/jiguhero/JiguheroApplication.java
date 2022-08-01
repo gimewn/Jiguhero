6a2d30d964455b053b5dff220b909921d29e768b
dnrjs8185 <dnrjs8185@naver.com>
2022-08-01 16:45:03 +0900

    merge

--- BACK/jiguhero/src/main/java/com/ssafy/jiguhero/JiguheroApplication.java
+++ BACK/jiguhero/src/main/java/com/ssafy/jiguhero/JiguheroApplication.java
@@ -10,7 +10,6 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 public class JiguheroApplication {
 
 	public static void main(String[] args) {
-		System.out.println();
 		SpringApplication.run(JiguheroApplication.class, args);
 	}
 
