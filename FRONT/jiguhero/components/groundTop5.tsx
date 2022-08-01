614f74bc21c5452ab4303d388a0703e56cc18a9c
dnrjs8185 <dnrjs8185@naver.com>
2022-08-01 17:23:23 +0900

    .

---
+++ FRONT/jiguhero/components/groundTop5.tsx
@@ -0,0 +1,40 @@
+import styled from 'styled-components';
+
+const Ground = styled('div')`
+    border-radius: 20px;
+    border: solid 1px #65ACE2;
+    display:flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    width:100%;
+    /* min-width:100px; */
+    height:120px;
+    padding:10px;
+    margin-right:20px;
+`
+const Icon = styled('p')`
+    font-size:20px;
+    margin: 5px auto;
+`
+const Title = styled('p')`
+    margin: 0 auto;
+    text-align: center;
+    font-size:13px;
+    width:80px;
+    word-break: keep-all;
+`
+
+interface GroundFiveProps{
+    icon:string,
+    title:string
+}
+
+export default function GroundFive({icon, title}:GroundFiveProps){
+    return(
+        <Ground>
+            <Icon>{icon}</Icon>
+            <Title>{title}</Title>
+        </Ground>
+    )
+}
