1904d34a45d8dea8dadb5bfc1f76d9182402b1f5
kian6365 <kian6365@naver.com>
2022-08-02 11:54:56 +0900

    BE/fix merge

--- BACK/jiguhero/src/main/java/com/ssafy/jiguhero/JiguheroApplication.java
+++ BACK/jiguhero/src/main/java/com/ssafy/jiguhero/JiguheroApplication.java
@@ -1,8 +1,10 @@
 package com.ssafy.jiguhero;
 
 
+import com.ssafy.jiguhero.config.FileUploadProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
@@ -11,6 +13,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @PropertySource(value = { "classpath:database/database.properties" })
 @PropertySource(value = { "classpath:oauth2/oauth2.properties" })
 @PropertySource(value = { "classpath:swagger/springdoc.properties" })
+@EnableConfigurationProperties({FileUploadProperties.class})
 public class JiguheroApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(JiguheroApplication.class, args);
