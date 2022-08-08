bb46e10df058ae392f86adab29e59fd5ab14f259
Yeongseo Kim <dudtjakdl@naver.com>
2022-08-08 15:53:18 +0900

    임무 이미지 불러오기 기능 추가

--- BACK/jiguhero/src/main/java/com/ssafy/jiguhero/data/dto/MissionDto.java
+++ BACK/jiguhero/src/main/java/com/ssafy/jiguhero/data/dto/MissionDto.java
@@ -30,6 +30,7 @@ public class MissionDto {
     private int failedPerson;
     private int likes;
     private int hits;
+    private String repImageURL;
     private List<String> imageURL = new ArrayList<>();
 
     private boolean likeCheck;
