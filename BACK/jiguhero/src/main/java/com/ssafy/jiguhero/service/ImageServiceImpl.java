cd6a45feca868d080161a61a68e690fc95b65e19
Yeongseo Kim <dudtjakdl@naver.com>
2022-08-11 10:01:08 +0900

    유저 이미지 업로드 반환값 수정

--- BACK/jiguhero/src/main/java/com/ssafy/jiguhero/service/ImageServiceImpl.java
+++ BACK/jiguhero/src/main/java/com/ssafy/jiguhero/service/ImageServiceImpl.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.annotation.PostConstruct;
+import javax.servlet.http.HttpServletRequest;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
@@ -76,12 +77,13 @@ public class ImageServiceImpl implements ImageService {
         map.put("origin_file", originalFileName);
         map.put("save_file", saveFileName);
         map.put("save_folder", File.separator + target + File.separator + today);
+        map.put("date", today);
 
         return map;
     }
 
     @Override
-    public String saveUserImage(MultipartFile file, Long userId) {
+    public String saveUserImage(MultipartFile file, Long userId, HttpServletRequest request) {
         // 이미 프로필 이미지가 등록되어 있는지 확인
         User user = userDao.selectUserById(userId);
         Image_User imageUser = imageDao.selectImageUser(user);
@@ -102,7 +104,9 @@ public class ImageServiceImpl implements ImageService {
         newImageUser.setUser(userDao.selectUserById(userId));
         imageDao.insertImageUser(newImageUser);
 
-        return map.get("save_file");
+        String url = request.getRequestURL().toString().replace(request.getRequestURI(),"") + "/image/" + map.get("save_file") + "?target=user&date=" + map.get("date");
+
+        return url;
     }
 
     @Override
