4d020fde4906eef43b027022156fd0af176654bf
dnrjs8185 <dnrjs8185@naver.com>
2022-08-13 15:39:59 +0900

    update

--- BACK/jiguhero/src/main/java/com/ssafy/jiguhero/controller/MissionController.java
+++ BACK/jiguhero/src/main/java/com/ssafy/jiguhero/controller/MissionController.java
@@ -43,7 +43,7 @@ public class MissionController {
         return ResponseEntity.status(HttpStatus.OK).body(list);
     }
 
-    @ApiOperation(value = "전체 임무 리스트를 반환한다.", response = List.class)
+    @ApiOperation(value = "전체 임무 리스트를 제목순(title) or 조회순(hits) or 등록순(time)으로 반환한다.", response = List.class)
     @GetMapping()
     public ResponseEntity<List<MissionDto>> getAllMissions(HttpServletRequest request, @RequestParam("array") String array) {
         List<MissionDto> list = missionService.getAllMissions(request, array);
@@ -51,7 +51,7 @@ public class MissionController {
         return ResponseEntity.status(HttpStatus.OK).body(list);
     }
 
-    @ApiOperation(value = "해당 임무의 모든 정보를 반환한다.", response = String.class) // 좋아요 클릭 여부/ 기능 추가해야 함 o
+    @ApiOperation(value = "임무를 상세보기 한다.", response = String.class) // 좋아요 클릭 여부/ 기능 추가해야 함 o
     @GetMapping("/{mission_id}/details")
     public ResponseEntity<MissionDto> getMission(@PathVariable("mission_id") Long missionId, @RequestParam("userId") Long userId, HttpServletRequest request) {
         MissionDto result = missionService.getMissionById(missionId, userId, request);
@@ -59,20 +59,19 @@ public class MissionController {
         return ResponseEntity.status(HttpStatus.OK).body(result);
     }
 
-    @ApiOperation(value = "새로운 임무를 등록한다.", response = String.class) // missionServiceImpl 확인!!! Conn_Mission도 추가해야함(임무 작성한 대원 저장 등)
+    @ApiOperation(value = "임무를 등록한다.", response = String.class)
     @PostMapping
     public ResponseEntity<String> saveMission(@RequestBody MissionDto missionDto, @RequestParam("userId") Long userId) {
-
         missionService.insertMission(missionDto, userId);
 
         return new ResponseEntity<String>("success", HttpStatus.OK);
-        // missionServiceImpl 확인!!! Conn_Mission도 추가해야함(임무 작성한 대원 저장 등)
     }
 
-    @ApiOperation(value = "해당 임무에 참여한다.", response = String.class)
+    @ApiOperation(value = "임무에 참여한다.", response = String.class)
     @PostMapping("/{mission_id}/details")
-    public ResponseEntity<String> saveMission(@RequestParam("userId") Long userId, @RequestParam("missionId") Long missionId) {
+    public ResponseEntity<String> joinMission(@RequestParam("userId") Long userId, @RequestParam("missionId") Long missionId) {
         int check = missionService.joinMission(userId, missionId);
+
         if(check == 1) {
             return new ResponseEntity<String>("success", HttpStatus.OK);
         }
@@ -81,16 +80,20 @@ public class MissionController {
         }
     }
 
-    @ApiOperation(value = "해당 임무의 '좋아요'를 클릭한다.", response = List.class)
+    @ApiOperation(value = "임무의 '좋아요'를 클릭한다.", response = List.class)
     @PostMapping("/{mission_id}/hearts")
     public ResponseEntity<String> saveLikeMission(@PathVariable("mission_id") Long missionId, @RequestParam("userId") Long userId) {
         int check = missionService.likeMission(missionId, userId);
 
-        if(check == 1) return new ResponseEntity<String>("success", HttpStatus.OK);
-        else return new ResponseEntity<String>("deletesuccess", HttpStatus.OK);
+        if(check == 1) {
+            return new ResponseEntity<String>("success", HttpStatus.OK);
+        }
+        else {
+            return new ResponseEntity<String>("deletesuccess", HttpStatus.OK);
+        }
     }
 
-    @ApiOperation(value = "해당 임무를 삭제한다.", response = List.class)
+    @ApiOperation(value = "임무를 삭제한다.", response = List.class)
     @DeleteMapping("/{mission_id}/details")
     public ResponseEntity<String> deleteMission(@PathVariable("mission_id") Long missionId, @RequestParam("userId") Long userId) {
         int check = missionService.deleteMission(missionId, userId);
@@ -101,21 +104,21 @@ public class MissionController {
         else {
             return new ResponseEntity<String>("fail", HttpStatus.OK);
         }
-
     }
 
-    @ApiOperation(value = "해당 임무의 세부 내용을 변경한다", response = String.class)
+    @ApiOperation(value = "임무의 세부 내용을 변경한다", response = String.class)
     @PutMapping("/{mission_id}/details")
     public ResponseEntity<MissionDto> updateMission(@RequestBody MissionDto missionDto, @RequestParam("user_id") Long userId) {
         MissionDto missionDtoResult = null;
+
         try {
             missionDtoResult = missionService.updateMission(missionDto, userId);
         }
         catch (Exception e) {
             throw new RuntimeException(e);
         }
-        return ResponseEntity.status(HttpStatus.OK).body(missionDtoResult);
 
+        return ResponseEntity.status(HttpStatus.OK).body(missionDtoResult);
     }
 
     /*
@@ -156,11 +159,9 @@ public class MissionController {
     @ApiOperation(value = "검색어로 검색한 임무 리스트 목록을 제목순(title) or 조회순(hits) or 등록순(time)으로 반환한다")
     @GetMapping("/search")
     public ResponseEntity<List<MissionDto>> searchMission(@RequestParam("search") String search, @RequestParam("array") String array){
-
         List<MissionDto> list = missionService.searchMission(search, array);
 
         return ResponseEntity.status(HttpStatus.OK).body(list);
-
     }
 
 }
